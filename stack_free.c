9b0f8bb5571d06a5fafa9fe8a8a618d90ba5823d
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-17 09:44:53 -0600

    Added 3 Files for reference

---
+++ stack_free.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * stack_free - frees the stack
+ * @stack: pointer to the top of the stack
+ * Return: void
+ */
+void stack_free(stack_t *stack)
+{
+    stack_t *temp;
+
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+}
