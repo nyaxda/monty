9b2596a17fcfee12546bcff7a02b492a806b0a70
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-17 11:43:55 -0600

    Removed the double free in main.c and added pall.c

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * o_pall - prints all the values on the stack, starting from the top
+ * @stack: pointer to the top of the stack
+ * @counter: line counter
+ * Return: void
+ */
+void o_pall(stack_t **stack, __attribute__((unused)) unsigned int counter)
+{
+    stack_t *temp;
+
+    temp = *stack;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
+}
