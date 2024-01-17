2d87f5233f35c24d9b3facca66aed23944216d14
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-17 12:23:16 -0600

    add nop functionality

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *o_nop- does absolutely nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void o_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
