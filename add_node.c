2885f5ffb8cacd66a136aa514a015b3b2c1693e7
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-17 11:21:35 -0600

    Added add_node.c file

---
+++ add_node.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the head of the stack
+ * @n: integer to be added
+ * Return: the address of the new element, or NULL if it failed
+ */
+void add_dnodeint(stack_t **head, int n)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		fclose(var.file);
+		free(var.data);
+		stack_free(*head);
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	if (temp != NULL)
+		temp->prev = new_node;
+	new_node->next = temp;
+	new_node->prev = NULL;
+	*head = new_node;
+}
