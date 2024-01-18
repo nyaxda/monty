bf5c4298fc64d9ed5448b22310c53cfde3b2474c
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-18 11:03:50 -0600

    Refactor add_dnodeint_end function to add nodes at the end of a tail stack_t list

--- add_node_end.c
+++ add_node_end.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * add_dnodeint_end - adds a new node at the end of a tail stack_t list
  * @head: pointer to the head of the stack
  * @n: integer to be added
  * Return: void
@@ -15,21 +15,22 @@ void add_dnodeint_end(stack_t **head, int n)
 	if (new_node == NULL)
 	{
 		printf("Error: malloc failed\n");
-		exit(0);
 	}
 	new_node->n = n;
+	new_node->next = NULL;
 	if (temp != NULL)
 	{
 		while (temp->next != NULL)
 			temp = temp->next;
-		temp->next = new_node;
-		new_node->prev = temp;
-		new_node->next = NULL;
+	}
+	if (temp == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
 	}
 	else
 	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*head = new_node;
+		new_node->prev = temp;
+		temp->next = new_node;
 	}
 }
